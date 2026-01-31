743f26f5407c3f18f32de73e89e7951823cc0978
Yogesh Bohra <yogeshbohra124@gmail.com>
2026-01-31 17:09:10 +0530

    update

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://demo-13r2.vercel.app/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://demo-13r2.vercel.app/api",
+  // baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
